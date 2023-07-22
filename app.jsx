7031da00aad84d7a53209cdfbfb03adbf2b237d1
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-22 16:22:14 +0530

    day 5

--- app.jsx
+++ app.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX
-// its functional component
-const Heading = () => (
-    <h1 id="title" key="h2">Hello Everynan</h1>
-);
-// its react Element
 const Title = (
-    <h1 id="title" key="h2">Hello Everynan</h1>
+    <h1 id="title" key="h2">Food Vila</h1>
 );
-let hei = 9 + 8;
 
 // this is component Composition
 const Header = () => {
     return (
         <>
-        {/* inside prenthesis we can write any js code */}
-        {hei}
-            {/* to call function */}
-            {Heading()}
 
-            {/* to call react element */}
-            {Title}
-            <h1>Hello functional component</h1>
         </>
     )
 };
