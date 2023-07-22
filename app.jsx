cd8eaac73fda45e57cb96b2a83be16aeb7092e18
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-22 16:49:59 +0530

    nav complete

--- app.jsx
+++ app.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const Title = (
-    <h1 id="title" key="h2">Food Vila</h1>
+// import Style from 'style.css'
+const Title = () => (
+    <a href="/"><img className="logo" src="https://cdn.octopix.in/uploads/company-logo/2020/11/19/food-villa-pSJVhwoN8KxgwV9jtuB1MlosJ0ejoKfiBiVO1jJPLM61shyarbxVvjIFy3DVpbUML8eBxcUo7BOWXQcd-350x350.jpg" alt="logo" /></a>
 );
 
-// this is component Composition
-const Header = () => {
+const HeaderComponent = () => {
     return (
+        <header>
+            {Title()}
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+const Body = () =>{
+
+}
+const AppLayout = () => {
+    return (
+        // react.fragment
         <>
-{Title()}
+            {HeaderComponent()}
         </>
     )
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Header />)
+root.render(<AppLayout />)
+
+
+
+/***
+ * jsx can only have one parent
+ * react.fragment is like an empty tag
+ */
