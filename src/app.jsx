f7a9779ec149cbb0653b1cc4a3a86d012ba8de95
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-25 11:20:58 +0530

    day 6

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import Style from 'style.css'
-const Title = () => (
-    <a href="/"><img className="logo" src="https://cdn.octopix.in/uploads/company-logo/2020/11/19/food-villa-pSJVhwoN8KxgwV9jtuB1MlosJ0ejoKfiBiVO1jJPLM61shyarbxVvjIFy3DVpbUML8eBxcUo7BOWXQcd-350x350.jpg" alt="logo" /></a>
-);
-
+import Title from "./component/Title";
 const HeaderComponent = () => {
     return (
         <header>
